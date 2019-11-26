97bd97c698382b19f68689f450732f525be92e94
yousef2213 <mandooyousef988@gmail.com>
2019-11-26 16:53:48 +0200

    Desigin Mobile

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+//function show() {
+//    document.getElementById('homeclick').style.background = 'url(Team1.jpg)';
+//    document.getElementById('homeclick').style.backgroundSize = 'cover';
+//    document.getElementById('homeclick').style.backgroundPosition = 'top';
+//    
+//}
+//function hiden() {
+//    document.getElementById('homeclick').style.background = '#262626';
+//    document.getElementById('homeclick').style.backgroundSize = '';
+//    
+//}
+
+
+$("#clc").click(function(){
+   $("#homeclick").toggleClass("let")
+});
